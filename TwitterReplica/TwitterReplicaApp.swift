15c22ff02a736ec51b9dc7954b439fc548429dfb
Nitin Kumar <kumarnitin5814@gmail.com>
2025-02-13 01:53:01 +0530

    Initial Commit

---
+++ TwitterReplica/TwitterReplicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterReplicaApp.swift
+//  TwitterReplica
+//
+//  Created by Nitin on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TwitterReplicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
