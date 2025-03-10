4bd3f3b683e8e0868d9f23763af13b62caa93649
Nitin Kumar <kumarnitin5814@gmail.com>
2025-03-10 23:32:22 +0530

    push

---
+++ AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  VistsDemo
+//
+//  Created by Nitin on 04/03/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
